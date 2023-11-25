567a6e0b6a81cccc41ddc3063f1b05f1f149e570
Arame Hovhannisyan <narhov123456@gmail.com>
2023-11-25 21:11:19 +0400

    add,edit,del news

--- controllers/news.js
+++ controllers/news.js
@@ -1,24 +1,79 @@
-import { addNews } from "../lib/newsM.js"
+import { addNews, deleteNews, editNews } from "../lib/newsM.js"
 import response from "../lib/response.js"
 import verifyToken from "../lib/verifyToken.js"
 
-export async function createNewsController(req, res){
+export async function createNewsController(req, res) {
     const responseObj = response()
     const token = req.headers.authorization.split(" ")[1]
-    console.log(token);
     const verifyiedToken = verifyToken(token)
-    console.log(verifyiedToken);
-    if(verifyiedToken !== null){
-
+    if (verifyiedToken !== null) {
         await addNews(req.body)
         responseObj.data = {
-            message : "News added succesfully"
+            message: "News added succesfully"
         }
         res.status(200).json(responseObj)
         return
     }
     responseObj.error = {
-        message : "forbidden"
+        message: "forbidden"
+    }
+    res.status(403).json(responseObj)
+
+}
+
+export async function editNewsController(req, res) {
+    const responseObj = response()
+    const token = req.headers.authorization.split(" ")[1]
+    const verifyiedToken = verifyToken(token)
+
+    if (verifyiedToken !== null) {
+        const id = req.params.id
+
+        const edit = await editNews(req.body, id)
+        if (edit == null) {
+            responseObj.error = {
+                message: "id is false"
+            }
+            res.status(402).json(responseObj)
+            return
+        }
+        responseObj.data = {
+            message: "News edited succesfully"
+        }
+        res.status(200).json(responseObj)
+        return
+    }
+    responseObj.error = {
+        message: "forbidden"
+    }
+    res.status(403).json(responseObj)
+
+}
+
+export async function deleteNewsController(req, res){
+    const responseObj = response()
+    const token = req.headers.authorization.split(" ")[1]
+    const verifyiedToken = verifyToken(token)
+
+    if (verifyiedToken !== null) {
+        const id = req.params.id
+
+        const deleteN = await deleteNews(id)
+        if (deleteN == null) {
+            responseObj.error = {
+                message: "id is false"
+            }
+            res.status(402).json(responseObj)
+            return
+        }
+        responseObj.data = {
+            message: "News deleted succesfully"
+        }
+        res.status(200).json(responseObj)
+        return
+    }
+    responseObj.error = {
+        message: "forbidden"
     }
     res.status(403).json(responseObj)
 
--- routs/news.js
+++ routs/news.js
@@ -1,9 +1,13 @@
 import express from "express"
-import {createNewsController} from "../controllers/news.js"
+import {createNewsController, deleteNewsController, editNewsController} from "../controllers/news.js"
 import validate from "../middleware/validate.js"
 
 const newsRouter = express.Router()
 
 newsRouter.post("/", validate("createNews"),createNewsController)
+
+newsRouter.put("/:id", validate("createNews"), editNewsController)
+
+newsRouter.delete("/:id", deleteNewsController)
 
 export default newsRouter
